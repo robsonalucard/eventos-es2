d3fe1ff05548aca9bbabd55ab6cd44d1ae79e6e1
Isaias-ml <isaias-ml@hotmail.com>
2018-04-23 21:51:15 -0300

    Modificações me base.html e criação de template de cadastro de evento(novo_evento.html)

--- cEventos/cEventos/eventos/urls.py
+++ cEventos/cEventos/eventos/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^lista-eventos/', views.lista_eventos, name='lista_eventos'),
+    url(r'^novo-evento/', views.novo_evento, name='novo_evento'),
 ]
--- cEventos/cEventos/eventos/views.py
+++ cEventos/cEventos/eventos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .forms import EventoForm
 from .models import Evento
@@ -9,3 +9,16 @@ from .models import Evento
 def lista_eventos(request):
     eventos = Evento.objects.all()
     return render(request, 'eventos/lista_eventos.html', {'eventos': eventos})
+
+
+def novo_evento(request):
+    if request.method == 'POST':
+        form = EventoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('eventos:lista_eventos')
+        else:
+            print(form.errors)
+    else:
+        form = EventoForm()
+    return render(request, 'eventos/novo_evento.html', {'form': form})
