92d944080ef5581b7eecb7edbc8f760a65da2328
Isaias-ml <isaias-ml@hotmail.com>
2018-05-12 11:57:52 -0300

    Finalização da view e do template de ediçao de evento

--- cEventos/cEventos/eventos/urls.py
+++ cEventos/cEventos/eventos/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^lista-eventos/', views.lista_eventos, name='lista_eventos'),
+    url(r'^$', views.lista_eventos, name='lista_eventos'),
     url(r'^novo-evento/', views.novo_evento, name='novo_evento'),
+    url(r'^editar-evento/(?P<id_evento>[0-9]+)/$',
+        views.editar_evento, name='editar_evento'),
 ]
--- cEventos/cEventos/eventos/views.py
+++ cEventos/cEventos/eventos/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 
 from .forms import EventoForm
 from .models import Evento
@@ -15,10 +16,42 @@ def novo_evento(request):
     if request.method == 'POST':
         form = EventoForm(request.POST)
         if form.is_valid():
-            form.save()
+            f = form.save(commit=False)
+            if compara_datas(f.data_inicial, f.data_final):
+                messages.error(
+                    request, 'Data Final deve ser posterior ou igual à Data Inicial.')
+                return render(request, 'eventos/novo_evento.html')
+            else:
+                f.save()
             return redirect('eventos:lista_eventos')
         else:
-            print(form.errors)
+            messages.error(
+                request, form.errors)
+            return render(request, 'eventos/novo_evento.html', {'form': form})
     else:
         form = EventoForm()
     return render(request, 'eventos/novo_evento.html', {'form': form})
+
+
+def editar_evento(request, id_evento):
+    evento = get_object_or_404(Evento, id=id_evento)
+    if request.method == 'POST':
+        form = EventoForm(request.POST, instance=evento)
+        if form.is_valid():
+            f = form.save(commit=False)
+            if compara_datas(f.data_inicial, f.data_final):
+                messages.error(
+                    request, 'Data Final deve ser posterior ou igual à Data Inicial.')
+                return render(request, 'eventos/novo_evento.html', {'form': form})
+            else:
+                f.save()
+            return redirect('eventos:lista_eventos')
+    else:
+        form = EventoForm(instance=evento)
+    return render(request, 'eventos/novo_evento.html', {'form': form})
+
+
+def compara_datas(valor1, valor2):
+    if valor1 > valor2:
+        return True
+    return False
