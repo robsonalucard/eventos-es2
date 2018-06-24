8f347dc4ca3d33ab69a56f30fc1784d1e17283b7
Isaias-ml <isaias-ml@hotmail.com>
2018-06-24 18:01:37 -0300

    Finalização de view e template de evento_profile

--- cEventos/cEventos/eventos/urls.py
+++ cEventos/cEventos/eventos/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
         views.editar_evento, name='editar_evento'),
     url(r'^evento/(?P<id_evento>[0-9]+)/$',
         views.evento_profile, name='evento_profile'),
+    url(r'^evento-inscricao/(?P<id_evento>[0-9]+)/$',
+        views.inscricao_evento, name='inscricao_evento'),
     url(r'^evento_busca/$',
         views.busca_evento, name='busca_evento'),
 ]
--- cEventos/cEventos/eventos/views.py
+++ cEventos/cEventos/eventos/views.py
@@ -2,6 +2,8 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 from django.db.models import Q
+from datetime import date
+from django.utils.safestring import mark_safe
 
 from .forms import EventoForm
 from .models import Evento, Inscricao
@@ -67,20 +69,57 @@ def editar_evento(request, id_evento):
 
 def evento_profile(request, id_evento):
     evento = get_object_or_404(Evento, id=id_evento)
+    if request.user.is_authenticated():
+        suas_inscricoes = Inscricao.objects.filter(user=request.user)
+        if suas_inscricoes:
+            eventos_inscritos = Evento.objects.filter(
+                id__in=[e.evento.id for e in suas_inscricoes])
+            for e in eventos_inscritos:
+                if e.id == evento.id:
+                    inscrito = True
+                    break
+                else:
+                    inscrito = False
+        else:
+            inscrito = False
+    else:
+        messages.info(
+            request, mark_safe('Para se inscrever, faça <a href="../../../accounts/login-usuario/" class="alert-link">Login</a> ou <a href="../../../accounts/novo-usuario/" class="alert-link">Cadastre-se</a>.'))
+        inscrito = False
     return render(
         request,
         'eventos/evento_profile.html',
         {
-            'evento': evento
+            'evento': evento,
+            'inscrito': inscrito,
         }
     )
 
 
 def busca_evento(request):
     id_busca = request.POST.get('busca', '')
+    # Query de busca no banco em varias colunas
     eventos = Evento.objects.filter(Q(titulo__icontains=id_busca) | Q(
         local__icontains=id_busca) | Q(cidade__icontains=id_busca) | Q(descricao__icontains=id_busca))
     return render(request, 'eventos/lista_busca.html', {'eventos': eventos})
+
+
+def inscricao_evento(request, id_evento):
+    evento = get_object_or_404(Evento, id=id_evento)
+    suas_inscricoes = Inscricao.objects.filter(user=request.user)
+    if evento.organizador != request.user:
+        inscricao = Inscricao(data_inscricao=date.today(),
+                              evento=id_evento, user=request.user)
+        inscricao.save()
+        redirect('eventos:evento_profile', id_evento=id_evento)
+    elif id_evento == [e.evento.id for e in suas_inscricoes]:
+        messages.error(
+            request, 'Você já esta inscrito nesse evento!')
+        return render(request, 'eventos/evento_profile.html')
+    elif evento.organizador == request.user:
+        messages.error(
+            request, 'O organizador do evento não nessicita de inscrição.')
+        return render(request, 'eventos/evento_profile.html')
 
 
 def compara_datas(valor1, valor2):
